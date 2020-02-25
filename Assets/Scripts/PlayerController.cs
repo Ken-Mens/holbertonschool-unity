42aa557b84262d5181a6ea9a5f283e6e4efe9a52
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-02-25 15:36:59 -0800

    add Player Controller and meta file

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+///<summary>Handles player control</summary>
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed = 500;
+
+
+    private void Start()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, speed * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -speed * Time.deltaTime);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(speed * Time.deltaTime, 0, 0);
+        }
+     }
+  }
