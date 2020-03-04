1db0b1d5ef57566a65c6292a07ad55fcebb487a3
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-03-03 18:30:15 -0800

    changed player controller script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,8 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 
 ///<summary>Handles player control</summary>
@@ -8,9 +11,13 @@ public class PlayerController : MonoBehaviour
     public Rigidbody rb;
     public float speed = 500;
     public int health = 5;
+    public Text healthText;
+    public Text scoreText;
     private int score = 0;
+    public Image winLoseBG;
 
 
+    public Text winLoseText;
 
     private void Start()
     {
@@ -42,27 +49,59 @@ public class PlayerController : MonoBehaviour
         if (other.tag == "Pickup")
         {
             score++;
+            SetScoreText();
             Debug.Log("Score: " + score);
             Destroy(other.gameObject);
         }
         if (other.tag == "Trap")
         {
             health--;
+            SetHealthText();
             Debug.Log("Health: " + health);
         }
         if (other.tag == "Goal")
         {
+            SetWin();
             Debug.Log("You win!");
         }
     }
-        void Update()
-        {
-            if (health == 0)
-            {
-                Debug.Log("Game Over!");
-                SceneManager.LoadScene("Maze", LoadSceneMode.Single);
-                score = 0;
-                health = 5;
-            }
-        }
+
+    void SetScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString();
     }
+
+    void SetHealthText()
+    {
+        healthText.text = "Health: " + health.ToString();
+    }
+
+
+    void SetWin()
+    {
+        winLoseBG.color = Color.green;
+        winLoseText.text = "You win!";
+        winLoseText.color = Color.black;
+    }
+
+
+		void Update()
+		{
+			if (health == 0)
+			{
+				winLoseBG.color = Color.red;
+				winLoseText.text = "Game Over!";
+				winLoseText.color = Color.white;
+            StartCoroutine(LoadScene(3));
+			}
+
+		}
+
+    private IEnumerator LoadScene(float seconds)
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene("Maze", LoadSceneMode.Single);
+        score = 0;
+        health = 5;
+    }
+}
