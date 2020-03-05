677165b1c50dc3a8893dff88fd01aeb97e2125e4
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-03-05 13:16:50 -0800

    add main menu script

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,13 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode;
 
-	// Use this for initialization
-	void Start () {
-		
+    static private bool cb_Mode;
+
+    // Use this for initialization
+    void Start () {
+        colorblindMode.isOn = cb_Mode;
 	}
 	
 	// Update is called once per frame
@@ -19,6 +25,18 @@ public class MainMenu : MonoBehaviour
 
     public void PlayMaze()
     {
+        cb_Mode = colorblindMode.isOn;
+        if (cb_Mode)
+        {
+            trapMat.color = new Color32(255, 112, 0, 1);
+            goalMat.color = Color.blue;
+        }
+
+        else
+        {
+            trapMat.color = Color.red;
+            goalMat.color = Color.green;
+        }
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
