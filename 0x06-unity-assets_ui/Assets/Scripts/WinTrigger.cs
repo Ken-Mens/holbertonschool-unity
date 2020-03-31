5df9ae9c5aed63bcd52e6a8ff5ea7b1d386f6a89
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-03-30 20:22:09 -0700

    add 0x06 directory unity assets ui

---
+++ 0x06-unity-assets_ui/Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+    ///<summary>Stops a timer</summary>
+    public class WinTrigger : MonoBehaviour
+    {
+        public Text timerText;
+
+        void OnTriggerEnter(Collider change)
+        {
+            if (change.name == "Player")
+            {
+                change.gameObject.GetComponent<Timer>().enabled = false;
+                timerText.fontSize = 84;
+                timerText.color = Color.green;
+            }
+        }
+    }
