312ad552e60ed5b5389f4b79721285dfaaa45a2d
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-04-08 16:17:52 -0700

    add camera controller script

--- 0x06-unity-assets_ui/Assets/Scripts/CameraController.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/CameraController.cs
@@ -6,31 +6,40 @@
 
         public float turn = 3f;
         public Transform player;
-        private Transform tsf;
-        /*
-        private Quaternion lockZ;
-        */
+        private Transform t;
+        public bool isInverted;
         private Vector3 offset;
         private Quaternion turnX;
         private Quaternion turnY;
 
-        void Awake()
-        {
-            tsf = GetComponent<Transform>();
-        }
-
-        void Start()
+    void Awake()
+    {
+        if (PlayerPrefs.GetInt("Y", 0) == 0)
+            isInverted = false;
+        else
+            isInverted = true;
+        t = GetComponent<Transform>();
+    }
+     
+    void Start()
         {
             Cursor.visible = false;
             offset = new Vector3(0, 1.25f, -6.25f);
         }
 
-        void LateUpdate()
+    void Update()
+    {
+        turnX = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turn, Vector3.up);
+        if (isInverted == true)
         {
-            turnX = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turn, Vector3.up);
-            turnY = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * turn, Vector3.left);
-            offset = turnX * turnY * offset;
-            transform.position = player.position + offset;
-            transform.LookAt(player.position + new Vector3(0, 0.24f, 0));
+            turnY = Quaternion.AngleAxis(-1 * (Input.GetAxis("Mouse Y") * turn), Vector3.left);
         }
+        else
+        {
+            turnY = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * turn, Vector3.left);
+        }
+        offset = turnX * turnY * offset;
+        transform.position = player.position + offset * Time.timeScale;
+        transform.LookAt(player.transform.position);
     }
+}
