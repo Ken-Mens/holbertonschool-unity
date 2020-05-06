cd09259252fe20ce8eb54e702ab8ea944db081e8
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-05-06 14:54:42 -0700

    add Trigger for audio on winflag gameobject

--- 0x08-unity-audio/Assets/Scripts/WinTrigger.cs
+++ 0x08-unity-audio/Assets/Scripts/WinTrigger.cs
@@ -11,16 +11,25 @@ public class WinTrigger : MonoBehaviour
   {
         public Text timerText;
         public Canvas WinCanvas;
+        public AudioSource winsource;
+        public AudioSource CheeryMonday;
+
+//    private void Awake()
+//    {
+//        winsource = GetComponent<AudioSource>();
+//        CheeryMonday = GetComponent<AudioSource>();
+//    }
 
     void OnTriggerEnter(Collider change)
         {
         if (change.name == "Player")
         {
-            // change.gameObject.GetComponent<Timer>().enabled = false;
-            // timerText.fontSize = 84;
-            // timerText.color = Color.green;
+            change.gameObject.GetComponent<Timer>().enabled = false;
+            timerText.fontSize = 84;
+            timerText.color = Color.green;
+            winsource.Play();
+            CheeryMonday.Stop();
             WinCanvas.gameObject.SetActive(true);
         }
       }
     }
-    
