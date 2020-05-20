74b543c4c20a89b20594b94dc0bb763d79eb8685
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-05-20 15:21:56 -0700

    add all required folders and assets

---
+++ 0x09-unity_ar_business_card/Assets/Scripts/OpenLink.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour
+{
+
+    public void Github_Link()
+    {
+        Application.OpenURL("https://github.com/Ken-Mens");
+    }
+
+    public void IN_Link()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/kenneth-mensah-07612238/");
+    }
+
+    public void TWitter_Link()
+    {
+        Application.OpenURL("https://twitter.com/Kmens5");
+    }
+
+    public void Email_Link()
+    {
+        Application.OpenURL("mailto:525@holbertonschool.com");
+    }
+}
+
